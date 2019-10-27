5a9332763e3a684b901db1d419d45efa1e2bdf48
rendongpo <B12050217@163.com>
2019-10-28 02:07:17 +0800

    Row && Column

---
+++ lib/demo/layout_demo.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(),
+          IconBadge(Icons.pool),
+          IconBadge(Icons.airplanemode_active, size: 50),
+          IconBadge(Icons.beach_access),
+        ],
+      ),
+    );
+  }
+}
+
+class IconBadge extends StatelessWidget {
+  final IconData icon;
+  final double size;
+
+  IconBadge(this.icon, {
+    this.size = 32.0
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(icon, size: size, color: Colors.white),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 33, 1.0),
+    );
+  }
+}
