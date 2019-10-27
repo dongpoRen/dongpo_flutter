cba96b51e84b8ff767094796e6dfa5b338e71bb4
rendongpo <rendongpo@chengfayun.com>
2019-10-27 23:58:43 +0800

    DrawerDemo

---
+++ lib/demo/drawer_demo.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text('dongpo', style: TextStyle(fontWeight: FontWeight.bold),),
+                accountEmail: Text('dongpe@163.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage('http://img3.redocn.com/tupian/20151015/weimeifengjingmanhuachatu_5091980.jpg'),
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.yellow[400],
+                  image: DecorationImage(
+                    image: NetworkImage('http://img3.redocn.com/tupian/20151015/weimeifengjingmanhuachatu_5091980.jpg'),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                      Colors.yellow,
+                      BlendMode.hardLight
+                    )
+                  )
+                ),
+              ),
+              ListTile(
+                title: Text('Messages', textAlign: TextAlign.right),
+                trailing: Icon(Icons.message, color: Colors.black12, size: 22.0),
+                onTap: () => Navigator.pop(context),
+              ),
+              ListTile(
+                title: Text('Favorite', textAlign: TextAlign.right),
+                trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0),
+                onTap: () => Navigator.pop(context),
+
+              ),
+              ListTile(
+                title: Text('Settings', textAlign: TextAlign.right),
+                trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0),
+                onTap: () => Navigator.pop(context),
+              ),
+            ],
+          ),
+        );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'demo/drawer_demo.dart';
 
 void main() => runApp(App());
 
@@ -55,47 +55,7 @@ class Home extends StatelessWidget {
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                accountName: Text('dongpo', style: TextStyle(fontWeight: FontWeight.bold),),
-                accountEmail: Text('dongpe@163.com'),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage('http://img3.redocn.com/tupian/20151015/weimeifengjingmanhuachatu_5091980.jpg'),
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.yellow[400],
-                  image: DecorationImage(
-                    image: NetworkImage('http://img3.redocn.com/tupian/20151015/weimeifengjingmanhuachatu_5091980.jpg'),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.yellow,
-                      BlendMode.hardLight
-                    )
-                  )
-                ),
-              ),
-              ListTile(
-                title: Text('Messages', textAlign: TextAlign.right),
-                trailing: Icon(Icons.message, color: Colors.black12, size: 22.0),
-                onTap: () => Navigator.pop(context),
-              ),
-              ListTile(
-                title: Text('Favorite', textAlign: TextAlign.right),
-                trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0),
-                onTap: () => Navigator.pop(context),
-
-              ),
-              ListTile(
-                title: Text('Settings', textAlign: TextAlign.right),
-                trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0),
-                onTap: () => Navigator.pop(context),
-              ),
-            ],
-          ),
-        )
+        drawer: DrawerDemo()
       ),
     );
   }
