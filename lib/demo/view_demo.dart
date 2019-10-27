c0d61948d351ba303adac5857d4b37f80f656edd
rendongpo <B12050217@163.com>
2019-10-28 03:25:02 +0800

    PageViewBuilder

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return PageViewBuilderDemo();
+  }
+}
+
+class PageViewBuilderDemo extends StatelessWidget {
+  
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(posts[index].title, style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(posts[index].author, style: TextStyle(fontWeight: FontWeight.bold)),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
+  }
+}
+
+class PageViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
@@ -9,25 +53,33 @@ class ViewDemo extends StatelessWidget {
       scrollDirection: Axis.vertical,
 
       onPageChanged: (currentPage) => debugPrint('Page: $currentPage'),
+      controller: PageController(
+        initialPage: 1,
+        // keepPage: false,
+        viewportFraction: 0.85,
+      ),
       children: <Widget>[
         Container(
           color: Colors.brown[900],
           alignment: Alignment.center,
-          child: Text('ONE',
+          child: Text(
+            'ONE',
             style: TextStyle(fontSize: 23.0, color: Colors.white),
           ),
         ),
         Container(
           color: Colors.grey[900],
           alignment: Alignment.center,
-          child: Text('TWO',
+          child: Text(
+            'TWO',
             style: TextStyle(fontSize: 23.0, color: Colors.white),
           ),
         ),
         Container(
           color: Colors.blueGrey[900],
           alignment: Alignment.center,
-          child: Text('THREE',
+          child: Text(
+            'THREE',
             style: TextStyle(fontSize: 23.0, color: Colors.white),
           ),
         ),
