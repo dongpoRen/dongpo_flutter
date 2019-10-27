2f45c1824f8c77e07cdd3d3bc5e0409f0c3e84dd
rendongpo <B12050217@163.com>
2019-10-28 02:39:32 +0800

    Stack

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,29 +7,57 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(
-            width: 200.0,
-            height: 300.0,
-            child: Container(
-              alignment: Alignment.bottomRight,
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 43, 53, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            alignment: Alignment.topLeft,
+            children: <Widget>[
+              SizedBox(
+                width: 200.0,
+                height: 300.0,
+                child: Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.green[300],
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Icon(Icons.ac_unit, color: Colors.white, size: 32),
+                ),
               ),
-              child: Icon(Icons.ac_unit, color: Colors.white, size: 32),
-            ),
-          ),
-          SizedBox(height: 32.0),
-          SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 43, 53, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+              SizedBox(height: 32.0),
+              SizedBox(
+                width: 100.0,
+                height: 100.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    // borderRadius: BorderRadius.circular(8.0),
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Colors.red,
+                        Colors.white
+                      ]
+                    )
+                  ),
+                  child:
+                      Icon(Icons.brightness_2, color: Colors.white, size: 32),
+                ),
               ),
-              child: Icon(Icons.brightness_2, color: Colors.white, size: 32),
-            ),
+              Positioned(
+                right: 20.0,
+                top: 20.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32),
+              ),
+              Positioned(
+                right: 150.0,
+                top: 100.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32),
+              ),
+              Positioned(
+                right: 100.0,
+                top: 200.0,
+                child: Icon(Icons.ac_unit, color: Colors.white, size: 32),
+              ),
+            ],
           )
         ],
       ),
@@ -41,9 +69,7 @@ class IconBadge extends StatelessWidget {
   final IconData icon;
   final double size;
 
-  IconBadge(this.icon, {
-    this.size = 32.0
-  });
+  IconBadge(this.icon, {this.size = 32.0});
 
   @override
   Widget build(BuildContext context) {
