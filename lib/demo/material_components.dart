4ad413ea18676463792d2ed8d194d30db908502e
rendongpo <B12050217@163.com>
2019-10-29 03:57:50 +0800

    配置snippets

---
+++ lib/demo/material_components.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MaterialCompoents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MaterialCompoents'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
