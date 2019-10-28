272b27bb83922d66708504750cd66d37f75630c6
rendongpo <B12050217@163.com>
2019-10-29 04:12:57 +0800

    FloatingActionButton

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -8,6 +8,45 @@ class MaterialCompoents extends StatelessWidget {
         title: Text('MaterialCompoents'),
         elevation: 0.0,
       ),
+      body: ListView(
+        children: <Widget>[
+          ListItem(title: 'FloatingActionButton', page: FloatingActionButtonDemo()),
+        ],
+      ),
+    );
+  }
+}
+
+class FloatingActionButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FloatingActionButtonDemo'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
+
+class ListItem extends StatelessWidget {
+  final String title;
+  final Widget page;
+
+  ListItem({
+    this.title,
+    this.page,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
     );
   }
 }
