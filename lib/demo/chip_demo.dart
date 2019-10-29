9e36f63054ffa8a812a9278bdeb814cebcb4a7d2
rendongpo <B12050217@163.com>
2019-10-30 03:22:12 +0800

    Chip: multi  delete

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -7,6 +7,13 @@ class ChipDemo extends StatefulWidget {
 }
 
 class _ChipDemoState extends State<ChipDemo> {
+
+  List<String> _tags = [
+    'Apple',
+    'Banana',
+    'Lemon'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,11 +63,36 @@ class _ChipDemoState extends State<ChipDemo> {
                   color: Colors.grey,
                   height: 32.0,
                   indent: 32.0,
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return Chip(
+                      label: Text(tag),
+                      onDeleted: () {
+                        setState(() {
+                          _tags.remove(tag);
+                        });
+                      },
+                    );
+                  }).toList(),
                 )
               ],
             )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.restore),
+        onPressed: () {
+          setState(() {
+            _tags = [
+              'Apple',
+              'Banana',
+              'Lemon'
+            ];
+          });
+        },
       ),
     );
   }
