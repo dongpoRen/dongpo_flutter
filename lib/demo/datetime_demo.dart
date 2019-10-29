7d5933f9c04fb5950af39c14ac0ba8e965f307f0
rendongpo <B12050217@163.com>
2019-10-30 01:11:57 +0800

    DateTime

--- lib/demo/datetime_demo.dart
+++ lib/demo/datetime_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DateTimeDemo extends StatefulWidget {
   @override
@@ -6,6 +7,16 @@ class DateTimeDemo extends StatefulWidget {
 }
 
 class _DateTimeDemoState extends State<DateTimeDemo> {
+  final DateTime selectedDate = DateTime.now();
+  void _selectDate() {
+    showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2300),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
    return Scaffold(
@@ -20,7 +31,17 @@ class _DateTimeDemoState extends State<DateTimeDemo> {
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[],
+              children: <Widget>[
+                InkWell(
+                  onTap: _selectDate,
+                  child: Row(
+                    children: <Widget>[
+                      Text(DateFormat.yMMMd().format(selectedDate)),
+                      Icon(Icons.arrow_drop_down),
+                    ],
+                  ),
+                )
+              ],
             )
           ],
         ),
