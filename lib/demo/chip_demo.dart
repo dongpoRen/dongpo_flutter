e81d74ebf0f5b8a3837de79012a022dab911c524
rendongpo <B12050217@163.com>
2019-10-30 03:33:23 +0800

    Chip: FilterChip

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -16,6 +16,10 @@ class _ChipDemoState extends State<ChipDemo> {
 
   String _actionChip = 'Nothing';
 
+  List<String> _selected = [
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -101,8 +105,35 @@ class _ChipDemoState extends State<ChipDemo> {
                     );
                   }).toList(),
                 ),
+                Divider(
+                  color: Colors.grey,
+                  height: 32.0,
+                  indent: 32.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  child: Text('FilterChip: ${_selected.toString()}'),
+                ),
+                Wrap(
+                  spacing: 8.0,
+                  children: _tags.map((tag) {
+                    return FilterChip(
+                      label: Text(tag),
+                      selected: _selected.contains(tag),
+                      onSelected: (value) {
+                        setState(() {
+                          if (_selected.contains(tag)) {
+                            _selected.remove(tag);
+                          } else {
+                            _selected.add(tag);
+                          }
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
@@ -115,6 +146,8 @@ class _ChipDemoState extends State<ChipDemo> {
               'Banana',
               'Lemon'
             ];
+
+            _selected = [];
           });
         },
       ),
