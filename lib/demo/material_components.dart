2c7011a6c49b7d0aeb7800952f29d33e0dcf4bb4
rendongpo <B12050217@163.com>
2019-10-30 00:36:38 +0800

    SwitchDemo

--- lib/demo/material_components.dart
+++ lib/demo/material_components.dart
@@ -5,6 +5,7 @@ import './popup_menu_button_demo.dart';
 import './form_demo.dart';
 import './checkbox_demo.dart';
 import './radio_demo.dart';
+import './switch_demo.dart';
 
 class MaterialCompoents extends StatelessWidget {
   @override
@@ -16,6 +17,7 @@ class MaterialCompoents extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListItem(title: 'Switch', page: SwitchDemo()),
           ListItem(title: 'Radio', page: RadioDemo()),
           ListItem(title: 'Checkbox', page: CheckboxDemo()),
           ListItem(title: 'Form', page: FormDemo()),
