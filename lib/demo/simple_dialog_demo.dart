56af6f200cc128fbbfd750028c51cfd91d7bfc7a
rendongpo <B12050217@163.com>
2019-10-30 01:47:14 +0800

    SimpleDialog

--- lib/demo/simple_dialog_demo.dart
+++ lib/demo/simple_dialog_demo.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+enum Option {
+  A, B, C
+}
 
 class SimpleDialogDemo extends StatefulWidget {
   @override
@@ -6,11 +11,64 @@ class SimpleDialogDemo extends StatefulWidget {
 }
 
 class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
+  String _choice = 'Nothing';
+
+  Future _openSimpleDialog() async{
+    final option = await showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Text('SimpleDialog'),
+          children: <Widget>[
+            SimpleDialogOption(
+              child: Text('Option A'),
+              onPressed: () {
+                Navigator.pop(context, Option.A);
+              },
+            ),
+            SimpleDialogOption(
+              child: Text('Option B'),
+              onPressed: () {
+                Navigator.pop(context, Option.B);
+              },
+            ),
+            SimpleDialogOption(
+              child: Text('Option C'),
+              onPressed: () {
+                Navigator.pop(context, Option.C);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    switch (option) {
+      case Option.A:
+        setState(() {
+          _choice = 'A';
+        });
+        break;
+      case Option.B:
+        setState(() {
+          _choice = 'B';
+        });
+        break;
+      case Option.C:
+        setState(() {
+          _choice = 'C';
+        });
+        break;
+      default:
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('WidgetDemo'),
+        title: Text('SimpleDialogDemo'),
         elevation: 0.0,
       ),
       body: Container(
@@ -18,12 +76,17 @@ class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('Your choice is $_choice'),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[],
             )
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.format_list_numbered),
+        onPressed: _openSimpleDialog,
       ),
     );
   }
