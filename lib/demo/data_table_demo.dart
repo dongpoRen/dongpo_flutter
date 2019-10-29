2df5dd3c135da07ec9ca7a043d1e50bcaa01031c
rendongpo <B12050217@163.com>
2019-10-30 04:04:02 +0800

    DataTable: sort

--- lib/demo/data_table_demo.dart
+++ lib/demo/data_table_demo.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import '../model/post.dart';
 
 class DataTableDemo extends StatefulWidget {
   @override
@@ -6,6 +8,10 @@ class DataTableDemo extends StatefulWidget {
 }
 
 class _DataTableDemoState extends State<DataTableDemo> {
+
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,34 +24,47 @@ class _DataTableDemoState extends State<DataTableDemo> {
         child: ListView(
           children: <Widget>[
             DataTable(
+              sortColumnIndex: _sortColumnIndex,
+              sortAscending: _sortAscending,
               columns: [
                 DataColumn(
                   label: Text('Title'),
+                  onSort: (int index, bool ascending) {
+                    setState(() {
+                      _sortColumnIndex = index;
+                      _sortAscending = ascending;
+
+                      posts.sort((a, b) {
+                        if (!ascending) {
+                          final c = a;
+                          a = b;
+                          b = c;
+                        }
+                        return a.title.length.compareTo(b.title.length);
+                      });
+                    });
+                  }
                 ),
                 DataColumn(
                   label: Text('Authors'),
                 ),
-              ],
-              rows: [
-                DataRow(
-                  cells: [
-                    DataCell(Text('hello~')),
-                    DataCell(Text('dongpo')),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text('holr ~')),
-                    DataCell(Text('dongpo')),
-                  ],
-                ),
-                DataRow(
-                  cells: [
-                    DataCell(Text('大家好~')),
-                    DataCell(Text('dongpo')),
-                  ],
+                // DataColumn(
+                //   label: Text('Description'),
+                // ),
+                DataColumn(
+                  label: Text('Image'),
                 ),
               ],
+              rows: posts.map((post) {
+                return DataRow(
+                  cells: [
+                    DataCell(Text(post.title)),
+                    DataCell(Text(post.author)),
+                    // DataCell(Text(post.description)),
+                    DataCell(Image.network(post.imageUrl)),
+                  ],
+                );
+              }).toList(),
             )
           ],
         ),
