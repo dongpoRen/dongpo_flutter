e8cc038772f2c52348c7c8a1671c4baa6f4257bf
rendongpo <B12050217@163.com>
2019-10-30 02:02:40 +0800

    BottomSheetDemo

---
+++ lib/demo/bottom_sheet_demo.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetDemo extends StatefulWidget {
+  @override
+  _BottomSheetDemoState createState() => _BottomSheetDemoState();
+}
+
+class _BottomSheetDemoState extends State<BottomSheetDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BottomSheetDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
